15ae44fe03daa52e33b28106851c9a684ce38032
Mattias <mattias@mediastrategi.se>
2015-06-29 16:58:17 +0200

    Hasher now pulls all data but missing logic for saving it into the sessions table

--- hasher.go
+++ hasher.go
@@ -35,25 +35,62 @@ func hashSessions(configuration Configuration) {
 
 		log.Println(end_time)
 
-		logsOut, err := db.Prepare("SELECT id, time, refid, participantid FROM `logs` ORDER BY `time` DESC, `index` DESC WHERE `time` > ?")
+		logsOut, err := db.Prepare("SELECT id, time, refid, participantid FROM `logs` WHERE `time` > ? ORDER BY `time` DESC, `index` DESC")
 		if err != nil {
 			log.Println(err.Error())
 		}
 		defer logsOut.Close()
+
+		logAttributesOut, err := db.Prepare("SELECT `key`, value FROM `log_attributes` WHERE log_id = ?")
+		if err != nil {
+			log.Println(err.Error())
+		}
+		defer logAttributesOut.Close()
+
+
+		sessionsIns, err := db.Prepare("INSERT INTO sessions VALUES( ?, ?, ?, ?, ?, ?, ? )")
+		if err != nil {
+			log.Println(err.Error())
+		}
+		defer sessionsIns.Close()
+
 
 		var (
 			sessionId int64
 			sessionTime time.Time
 			sessionRefid int64
 			sessionParticipantid int64
+			logAttributeKey string
+			logAttributeValue string
 		)
 
-		err = sessionsOut.QueryRow(end_time).Scan(&sessionId, &sessionTime, &sessionRefid, &sessionParticipantid)
+		logRows, err := logsOut.Query(end_time)
 		if err != nil {
 			log.Println(err.Error())
 		}
 
-		log.Printf("%d, %v, %d, %d", sessionId, sessionTime, sessionRefid, sessionParticipantid)
+		for logRows.Next() {
+			err = logRows.Scan(&sessionId, &sessionTime, &sessionRefid, &sessionParticipantid)
+			if err != nil {
+				log.Println(err.Error())
+			}
+
+			log.Printf("%d, %v, %d, %d", sessionId, sessionTime, sessionRefid, sessionParticipantid)
+
+			logAttributeRows, err := logAttributesOut.Query(sessionId)
+			if err != nil {
+				log.Println(err.Error())
+			}
+
+			for logAttributeRows.Next() {
+				err = logAttributeRows.Scan(&logAttributeKey, &logAttributeValue)
+				if err != nil {
+					log.Println(err.Error())
+				}
+
+				log.Printf("%s: %s", logAttributeKey, logAttributeValue)
+			}
+		}
 
 		elapsed := time.Since(start)
 		log.Printf("Hasing sessions took %s", elapsed)
